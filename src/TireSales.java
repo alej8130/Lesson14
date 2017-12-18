c3f5603aac1adffdde3e24cba0639c1743af9caa
alej8130 <alej8130@ISS2TWQKH2.scdsb.pri>
2017-12-18 10:26:19 -0500

    finished tire sales

--- src/TireSales.java
+++ src/TireSales.java
@@ -4,7 +4,7 @@ public class TireSales {
         //make a tire pole
         ArrayStack pole = new ArrayStack();
         Tire t;
-        System.out.println("Adding 10 goodyears to the pole");
+        System.out.println("Adding 10 Goodyears to the pole");
         for (int i = 0; i < 10; i++) {
             t=new Tire("Goodyear");
             pole.push(t);
@@ -14,6 +14,15 @@ public class TireSales {
             t= (Tire)pole.pop();
             System.out.println(t);
         }
+        System.out.println("Adding 2 new Cooper Tires to stack");
+        pole.push(new Tire("Cooper"));
+        pole.push(new Tire("Cooper"));
+        System.out.println("Selling another 4 tires");
+        for (int i = 0; i < 4; i++) {;
+            System.out.println(pole.pop());
+        }
+        System.out.println("Next tir to be sold is " + pole.peekTop());
+        System.out.println("Number of tires left on pole is " + pole.size());
     }
     
 }
